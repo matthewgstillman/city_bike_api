ded2f4b5314101b46d60a36e9a8cb339d7be9246
Matt <matthewgstillman@gmail.com>
2018-09-21 11:53:34 -0700

    Latest Commit

--- apps/city_bike_api/views.py
+++ apps/city_bike_api/views.py
@@ -3,10 +3,20 @@ import requests
 
 # Create your views here.
 def index(request):
+    network_list = []
     url = ("http://api.citybik.es/v2/networks")
     response = requests.get(url)
-    networks = response.json()
+    city_bikes = response.json()
+    networks = city_bikes['networks']
+    i = 0
+    for i in range(0, len(networks)):
+        print(networks[i])
+        network_list.append(networks[i])
+        i += 1
     context = {
+        # 'company': company,
+        'city_bikes': city_bikes,
+        'network_list': network_list,
         'networks': networks
     }
     return render(request, 'city_bike_api/index.html', context)
