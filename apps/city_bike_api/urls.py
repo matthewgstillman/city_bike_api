69a020cdb2b0d534d2670f2c3425f41a7aa6f716
Matt <matthewgstillman@gmail.com>
2018-09-22 14:21:38 -0700

    Added additional Funcionality

--- apps/city_bike_api/urls.py
+++ apps/city_bike_api/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 urlpatterns = [
     url(r'^$', views.index, name="index"),
-    url(r'^(?P<slug>[-\w]+)/$', views.company, name='company'),
+    url(r'^company$', views.company, name='company'),
+    url(r'^company/(?P<id>[-\w]+)/$', views.company_name, name='company_name'),
 ]
--- apps/city_bike_api/views.py
+++ apps/city_bike_api/views.py
@@ -25,6 +25,8 @@ def index(request):
         company_url = str(url_root) + str(href)
         request.session['company_url'] = company_url
         session_company_url = request.session['company_url']
+        request.session['href'] = href
+        session_href = request.session['href']
     print("Company ID Href Dictionary: " + str(company_id_href_dict))
     context = {
         'company_id_href_dict': company_id_href_dict,
@@ -32,8 +34,34 @@ def index(request):
         'network_list': network_list,
         'networks': networks,
         'session_company_url': session_company_url,
+        'session_href': session_href,
     }
     return render(request, 'city_bike_api/index.html', context)
 
 def company(request):
+    url = ("http://api.citybik.es/v2/networks/edinburgh-cycle-hire")
+    response = requests.get(url)
+    company = response.json()
+    print("Company: " + str(company))
+    network = company['network']
+    print("Network: " + str(network))
+    name = network['name']
+    print("Name: " + str(name))
+    i = 0
+    for i in range(0, len(network)):
+        station = network['stations'][i]
+        print("Station: " + str(station))
+        i += 1
+    context = {
+        'company': company,
+        'network': network,
+        'name': name,
+        'station': station,
+    }
+    return render(request, 'city_bike_api/company.html', context)
+
+def company_name(request):
+    url = ("http://api.citybik.es/v2/networks/v2/networks/edinburgh-cycle-hire")
+    context = {
+    }
     return render(request, 'city_bike_api/company.html', context)
